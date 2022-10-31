c3813c60b794f5cf9476d5f96924e14630c3c397
vitoriapinheiro <vps3@cin.ufpe.br>
2022-10-31 19:04:21 -0300

    implemented mechanics

--- ChallengeAthenas/ChallengeAthenas/Game/Beach.swift
+++ ChallengeAthenas/ChallengeAthenas/Game/Beach.swift
@@ -15,6 +15,21 @@ struct BeachView: View {
     @State var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     @State var pauseIsActive = false
     
+    @State var screenWidth = CGFloat(0)
+    @State var screenHeight = CGFloat(0)
+    
+    @State var crossPosition = CGPoint(x: 200, y: 300)
+    @State var crossSize = CGFloat(1)
+    
+    @State var hatPosition = CGPoint(x: 200, y: 300)
+    @State var hatSize = CGFloat(1)
+    
+    @State var starPosition = CGPoint(x: 200, y: 300)
+    @State var starSize = CGFloat(1)
+    
+    @State var fishbonePosition = CGPoint(x: 200, y: 300)
+    @State var fishboneSize = CGFloat(1)
+    
     var body: some View {
         GeometryReader{ geo in
             ZStack{
@@ -22,8 +37,8 @@ struct BeachView: View {
                     .resizable()
                     .ignoresSafeArea(.all)
                     .aspectRatio(contentMode: .fill)
+                
                 VStack{
-                    
                     Text("3:00")
                         .bold()
                     Spacer()
@@ -76,6 +91,42 @@ struct BeachView: View {
                 }
                 .padding(.top, 60)
                 
+                Image("CrossObj")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: crossSize, height: crossSize)
+                    .position(self.crossPosition)
+                    .onReceive(self.timer) { _ in
+                        self.crossMove()
+                    }
+                
+                Image("HatObj")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: hatSize, height: hatSize)
+                    .position(self.hatPosition)
+                    .onReceive(self.timer) { _ in
+                        self.hatMove()
+                    }
+                
+                Image("StarObj")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: starSize, height: starSize)
+                    .position(self.starPosition)
+                    .onReceive(self.timer) { _ in
+                        self.starMove()
+                    }
+                
+                Image("FishboneObj")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: fishboneSize, height: fishboneSize)
+                    .position(self.fishbonePosition)
+                    .onReceive(self.timer) { _ in
+                        self.fishboneMove()
+                    }
+                
                 PauseButtonView(sheetIsActive: $pauseIsActive)
                     .padding(.top, 60)
                 
@@ -84,8 +135,72 @@ struct BeachView: View {
                 }
             }
             .frame(width: geo.size.width, height: geo.size.height)
-            //            .onReceive(self.timer){ _ in self.collisionDetection()}
+                        .onReceive(self.timer){ _ in
+//                            self.collisionDetection()
+//                            updateScreenSize(w: geo.size.width, h: geo.size.height)
+                            self.screenWidth = geo.size.width
+                            self.screenHeight = geo.size.height
+                            
+                        }
         }
         .edgesIgnoringSafeArea(.all)
+    }
+    
+//    func updateScreenSize(w, h){
+//        self.screenWidth = w
+//        self.screenHeight = h
+//    }
+    
+    func crossMove() {
+        if (self.screenHeight - 130) > self.hatPosition.y{
+            withAnimation{
+                self.crossPosition.x -= 2.0
+                self.crossPosition.y += 4.5
+                self.crossSize += 0.85
+            }
+        } else {
+            self.crossPosition.x = 200
+            self.crossPosition.y = 300
+            self.crossSize = 1
+        }
+    }
+    func hatMove() {
+        if (self.screenHeight - 100) > self.hatPosition.y{
+            withAnimation{
+                self.hatPosition.x -= 0.5
+                self.hatPosition.y += 5
+                self.hatSize += 0.8
+            }
+        } else {
+            self.hatPosition.x = 200
+            self.hatPosition.y = 300
+            self.hatSize = 1
+        }
+    }
+    func starMove() {
+        if (self.screenHeight - 100) > self.starPosition.y{
+            withAnimation{
+                self.starPosition.x += 0.5
+                self.starPosition.y += 5
+                self.starSize += 0.8
+            }
+        } else {
+            self.starPosition.x = 200
+            self.starPosition.y = 300
+            self.starSize = 1
+        }
+    }
+    func fishboneMove() {
+        if (self.screenHeight - 130) > self.hatPosition.y{
+            withAnimation{
+                self.fishbonePosition.x += 1.8
+                self.fishbonePosition.y += 4
+                self.fishboneSize += 0.85
+            }
+        } else {
+            self.fishbonePosition.x = 200
+            self.fishbonePosition.y = 300
+            self.fishboneSize = 1
+        }
     }
 }
